2dea6f7ff2eab0d45e764a0de04f074c1521fa84
Asish Jacob <asishjac@gmail.com>
2024-06-13 22:47:25 +0200

    Remove @NoArgsConstructor.

--- src/main/java/com/score/model/Match.java
+++ src/main/java/com/score/model/Match.java
@@ -2,11 +2,9 @@ package com.score.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Match {
 
     private Team homeTeam;
--- src/main/java/com/score/model/Team.java
+++ src/main/java/com/score/model/Team.java
@@ -2,11 +2,9 @@ package com.score.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Team {
     private String name;
     private int goals;
